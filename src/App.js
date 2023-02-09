2c1946b6372b2d4edf299dc0c060c70d39732705
Vovchenko Olga <vovchenkoolga@gmail.com>
2023-02-09 19:42:05 +0200

    change in package

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import { useState,useEffect,useMemo,useCallback } from 'react';
 import Numbers from './child'
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 
 function App() {
 
@@ -10,15 +14,14 @@ const[name, setName]=useState('возвратиться');
 const[category,setCategory]=useState([]);
 const[count, setCount]=useState(0);
 const retro =useMemo(()=>{
-   setCategory(name==='возвратиться'?['любовь', 'жизнь', 'деньги','работа','здоровье']:['возвратиться']
+   setCategory(name==='возвратиться'
+   ?['любовь', 'жизнь', 'деньги','работа','здоровье']
+   :['возвратиться']
    )},[name]
    );
 const counter=useMemo(()=>{setCount(count+1)},[name]);
 const ran=useMemo(()=>(Math.floor(Math.random()*10)),[name]);
-useEffect(()=>{
-   console.log('on',name);
-   return ()=>{console.log('off', name)}
-},[name])
+useEffect(()=>{console.log(count)},[name])
 
 return (
 <body>
@@ -28,18 +31,39 @@ return (
      Гадание по книгам. Задай вопрос и нажми подходящую кнопку...
   </h1>
     </header>
-    <div className="pct">
-      <div className={name!=='возвратиться'?"Ans":"Noans"}>
+    
+        <div className="pct">
+        
+         
         <Numbers name={name} rand={ran} county={count}/>
-      </div>
+        
+    
+        
+
+      
+
+      
       <div>
        <ul>
-                
+           <TransitionGroup>    
       {category.map((cat,index)=>{
-         return(<li key={index}><img src="https://st.depositphotos.com/3369547/53198/v/600/depositphotos_531983490-stock-illustration-open-book-pages.jpg" className={(cat==='возвратиться')?"alfa"+1:"alfa"+index}/>
-            <button className={(cat==='возвратиться')?"alfa"+1:"alfa"+index} onClick={()=>setName(cat)}>{cat}</button></li>)
+        return(
+        <CSSTransition
+        key={index}
+        timeout={1800}
+        classNames="item">
+
+         <li>
+          <img 
+          src="https://st.depositphotos.com/3369547/53198/v/600/depositphotos_531983490-stock-illustration-open-book-pages.jpg" 
+          className={(cat==='возвратиться')?"alfa"+1:"alfa"+index}/>
+            <button 
+            className={(cat==='возвратиться')?"alfa"+1:"alfa"+index} 
+            onClick={()=>setName(cat)}>{cat}</button></li>
+            </CSSTransition>
+        )
       })} 
-          
+          </TransitionGroup>
       
        </ul>
       </div>
